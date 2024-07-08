92b5ba52f5f2eb8cf314f9b112e97bee0155a077
SuprLazr <ericrc87@gmail.com>
2024-07-08 12:10:04 -0400

    K: Wrote functions to convert BlockNodes to HTMLNodes.

--- src/blocknode.py
+++ src/blocknode.py
@@ -1,5 +1,7 @@
 
 from enum import Enum
+from htmlnode import ParentNode
+import re
 
 class Block_Type(Enum):
     paragraph = 1
@@ -42,7 +44,71 @@ def markdown_to_blocks(block_text):
     revised = []
     for line in split_new_line:
         revised.append(line.strip('\n'))
-    return block_type_convert(revised)
+    return blocknode_to_htmlnode(block_type_convert(revised))
+
+#all of these should be Parent Nodes because the leaf nodes may have more Markdown in them
+def blocknode_to_htmlnode(blocknode):
+    split_nodes = []
+    if isinstance(blocknode, BlockNode):
+        blocknode = [blocknode]
+    #first we need to make a <div> HTMLNode (Parent) and each child is a separate block
+    top_level = ParentNode("<div>", split_nodes)
+    for block in blocknode:
+        if block.type == Block_Type.unordered_list:
+            split_nodes.append(unordered_list_to_html(block.contents))
+        elif block.type == Block_Type.ordered_list:
+            split_nodes.append(ordered_list_to_html(block.contents))
+        elif block.type == Block_Type.code:
+            split_nodes.append(code_to_html(block.contents))
+        elif block.type == Block_Type.quote:
+            split_nodes.append(quotes_to_html(block.contents))
+        elif block.type == Block_Type.heading:
+            split_nodes.append(heading_to_html(block.contents))
+        else:
+            split_nodes.append(ParentNode("<p>", [block.contents]))
+    return top_level
+
+#<ul> and <li> are both parent nodes with content as children for further processing
+def unordered_list_to_html(blocknode):
+    node_list = []
+    top_level = ParentNode("<ul>", node_list)
+    for li in blocknode.split("*"):
+        if len(li) >= 2:
+            node_list.append(ParentNode("<li>", [li]))
+    #Parentnode <ul> has the <li> children
+    return top_level
+
+#<ol> and <li> are both parent nodes with content as children for further processing
+def ordered_list_to_html(blocknode):
+    node_list = []
+    top_level = ParentNode("<ol>", node_list)
+    for li in re.split("(\d+\.)",blocknode):
+        if len(li) > 2:
+            node_list.append(ParentNode("<li>", [li]))
+    #Parentnode <ol> has the <li> children
+    return top_level
+
+#content needs to be surrounded by <blockquote>
+def quotes_to_html(blocknode):
+    top_level = ParentNode("<blockquote>", [blocknode.strip(">")])
+    return top_level
+
+def code_to_html(blocknode):
+    top_level = ParentNode("<pre>", (ParentNode("<code>", [blocknode.strip("`")])))
+    return top_level
+
+#determine the number of # each heading has and then use <h1> - <h6>
+def heading_to_html(blocknode):
+    sep_head_level = blocknode.split(" ")
+    x = 0
+    for s in sep_head_level[0]:
+        if s == "#":
+            x+=1
+    if x <= 6:
+        top_level = ParentNode(f"<h{x}>", [blocknode.strip("#")])
+        return top_level
+    else:
+        return ParentNode("<p>", [blocknode])
     
 block_markdown = """
 ###### heading
--- src/convert_fun.py
+++ src/convert_fun.py
@@ -1,6 +1,6 @@
 from textnode import Text_Type, TextNode
 from htmlnode import HTMLNode,ParentNode,LeafNode
-from blocknode import BlockNode, Block_Type, block_type_convert, markdown_to_blocks
+from blocknode import BlockNode, Block_Type
 import re
 
 #helper function - splits text nodes into more text nodes based on Markdown syntax
@@ -139,52 +139,3 @@ def markdown_to_text_nodes(text):
             split_nodes.append(node)
         
     return split_nodes
-
-#HTMLNode reference sheet:
-#HTMLNode(tag, value, children, props)
-#tag = string representing the HTML tag name
-#value = string representing the value of the HTML tag, text inside p
-#children = list of HTMLNode objects representing the children of the node
-#props = dictionary of key:value pairs representing attributes of the HTML tag
-  
-def markdown_to_html_node(blocknode):
-    split_nodes = []
-    if isinstance(blocknode, BlockNode):
-        blocknode = [blocknode]
-    #first we need to make a <div> HTMLNode (Parent) and each child is a separate block
-    for block in blocknode:
-        if block.type == Block_Type.unordered_list:
-            return split_nodes.append(unordered_list_to_html(block))
-        
-#<ul>
- # <li>Neil Armstrong</li>
-  #<li>Alan Bean</li>
-  #<li>Peter Conrad</li>
-  #<li>Edgar Mitchell</li>
-  #<li>Alan Shepard</li>
-#</ul>
-#parent HTMLnode <ul> has the <li> children
-
-def unordered_list_to_html(blocknode):
-    #Parentnode <ul> has the <li> children
-    pass
-
-def ordered_list_to_html(blocknode):
-    #Parentnode <ol> has the <li> children
-    pass
-
-def quotes_to_html(blocknode):
-    #quote blocks should be surrounded by <blockquote> tag
-    pass
-
-def code_to_html(blocknode):
-    #code blocks have a <pre> ParentNode and <code> child(ren) node(s)
-    pass
-
-def heading_to_html(blocknode):
-    #determine the number of # each heading has and then use <h1> - <h6>
-    pass
-
-def paragraph_to_html(blocknode):
-    #ez pz just put <p> on it
-    pass
